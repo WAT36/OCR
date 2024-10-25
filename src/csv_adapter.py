f53e9c9b5404898c0d132b7078be6d335e37301d
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-10-25 23:19:21 +0900

    まず左半分を解析してCSVに落とすスクリプト作成

---
+++ src/csv_adapter.py
@@ -0,0 +1,54 @@
+import sys
+import os
+import re
+
+args = sys.argv
+
+if len(args) != 4:
+    print("Error: Usage: {0} (Input left-txt file) (Input right-txt file) (Output csv file)".format(args[0]))
+    sys.exit(1)
+
+left_txt_file = args[1]
+right_txt_file = args[2]
+output_csv_file = args[3]
+
+if(not os.path.isfile(left_txt_file)):
+    print("Error: {0} is not found.".format(left_txt_file))
+    sys.exit(1)
+elif(not os.path.isfile(right_txt_file)):
+    print("Error: {0} is not found.".format(right_txt_file))
+    sys.exit(1)
+
+empty_phrase = '-{3,20}'
+
+output_txt = []
+
+# reading left file
+with open(left_txt_file) as f:
+    question={'sentense': '','option':[]}
+    sentense = ''
+    for line in f:
+        if(re.search('-{3,20}',line)):
+            # 問題文データを登録
+            sentense=line+' '
+        elif(re.search(r'\([ABCD]',line)):
+            # 選択肢の場合
+            question['option'].append(re.sub(r'\([ABCD].* +','',line).replace('\n', ''))
+            if re.search(r'\(D',line):
+                #  (D)の場合はここで問題データ登録して初期化
+                output_txt.append(question['sentense']+','+','.join(question['option']))
+                sentense=''
+                question={'sentense': '','option':[]}
+        elif(line.strip() == ''):
+            # 空行の場合
+            if sentense != '':
+                # 問題文にデータがあるときは問題文を登録
+                question['sentense'] = sentense.replace('\n', '')
+                sentense = ''
+            continue
+        else:
+            sentense += line + ' '
+
+# csvファイルに書き込み
+with open(output_csv_file, mode='w') as f:
+    f.write('\n'.join(output_txt))
