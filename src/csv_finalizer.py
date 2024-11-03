69c06684e350bd2ee4c6a1b6830c38adf1bdc9e7
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2024-11-03 23:01:01 +0900

    最終変換での微調整

--- src/csv_finalizer.py
+++ src/csv_finalizer.py
@@ -33,27 +33,27 @@ with open(input_csv_file) as f:
         #解説　正解の場合は書き換えず消した方が良さげ？？
         if(csv_data[5]=='A'):
             csv_data.pop(5)
-            csv_data[-1]=csv_data[-1].replace('(A)','').replace('(A','').replace('A)','').replace('(B)',r'{d1}').replace('(B',r'{d1}').replace('B)',r'{d1}').replace('(C)',r'{d2}').replace('(C',r'{d2}').replace('C)',r'{d2}').replace('(D)',r'{d3}').replace('(D',r'{d3}').replace('D)',r'{d3}')
+            csv_data[-1]=csv_data[-1].replace('(A)',' ').replace('(A',' ').replace('A)',' ').replace('(B)',r'{d1} ').replace('(B',r'{d1} ').replace('B)',r'{d1} ').replace('(C)',r'{d2} ').replace('(C',r'{d2} ').replace('C)',r'{d2} ').replace('(D)',r'{d3} ').replace('(D',r'{d3} ').replace('D)',r'{d3} ')
         elif(csv_data[5]=='B'):
             csv_data.pop(5)
             answer=csv_data.pop(2)
             csv_data.insert(1,answer)
-            csv_data[-1]=csv_data[-1].replace('(B)','').replace('(B','').replace('B)','').replace('(A)',r'{d1}').replace('(A',r'{d1}').replace('A)',r'{d1}').replace('(C)',r'{d2}').replace('(C',r'{d2}').replace('C)',r'{d2}').replace('(D)',r'{d3}').replace('(D',r'{d3}').replace('D)',r'{d3}')
+            csv_data[-1]=csv_data[-1].replace('(B)',' ').replace('(B',' ').replace('B)',' ').replace('(A)',r'{d1} ').replace('(A',r'{d1} ').replace('A)',r'{d1} ').replace('(C)',r'{d2} ').replace('(C',r'{d2} ').replace('C)',r'{d2} ').replace('(D)',r'{d3} ').replace('(D',r'{d3} ').replace('D)',r'{d3} ')
         elif(csv_data[5]=='C'):
             csv_data.pop(5)
             answer=csv_data.pop(3)
             csv_data.insert(1,answer)
-            csv_data[-1]=csv_data[-1].replace('(C)','').replace('(C','').replace('C)','').replace('(A)',r'{d1}').replace('(A',r'{d1}').replace('A)',r'{d1}').replace('(B)',r'{d2}').replace('(B',r'{d2}').replace('B)',r'{d2}').replace('(D)',r'{d3}').replace('(D',r'{d3}').replace('D)',r'{d3}')
+            csv_data[-1]=csv_data[-1].replace('(C)',' ').replace('(C',' ').replace('C)',' ').replace('(A)',r'{d1} ').replace('(A',r'{d1} ').replace('A)',r'{d1} ').replace('(B)',r'{d2} ').replace('(B',r'{d2} ').replace('B)',r'{d2} ').replace('(D)',r'{d3} ').replace('(D',r'{d3} ').replace('D)',r'{d3} ')
         elif(csv_data[5]=='D'):
             csv_data.pop(5)
             answer=csv_data.pop(4)
             csv_data.insert(1,answer)
-            csv_data[-1]=csv_data[-1].replace('(D)','').replace('(D','').replace('D)','').replace('(A)',r'{d1}').replace('(A',r'{d1}').replace('A)',r'{d1}').replace('(B)',r'{d2}').replace('(B',r'{d2}').replace('B)',r'{d2}').replace('(C)',r'{d3}').replace('(C',r'{d3}').replace('C)',r'{d3}')
+            csv_data[-1]=csv_data[-1].replace('(D)',' ').replace('(D',' ').replace('D)',' ').replace('(A)',r'{d1} ').replace('(A',r'{d1} ').replace('A)',r'{d1} ').replace('(B)',r'{d2} ').replace('(B',r'{d2} ').replace('B)',r'{d2} ').replace('(C)',r'{d3} ').replace('(C',r'{d3} ').replace('C)',r'{d3} ')
         else:
             print("Error: Answer is not inproper value. {0}".format(csv_data[5]))
             sys.exit(1)
         # 問題ファイルIDを先頭に添える
-        csv_data.insert(0,8)
+        csv_data.insert(0,'8')
         output_txt.append(','.join(csv_data))
         index+=1
 
