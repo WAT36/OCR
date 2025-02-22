23ddb576b02b1697a9235caa28e95988e19c8596
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-02-22 09:42:51 +0900

    一旦インフラ全削除　当面アプリで使うのがいいと判断

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -20,34 +20,34 @@ export class InfraStack extends cdk.Stack {
       autoDeleteObjects: true,
     });
 
-    // ECR repository
-    const repository = new ecr.Repository(this, "OCRRepository", {
-      repositoryName: "ocr-repo",
-    });
+    // // ECR repository
+    // const repository = new ecr.Repository(this, "OCRRepository", {
+    //   repositoryName: "ocr-repo",
+    // });
 
-    // Lambda
-    const ocrLambda = new lambda.Function(this, "OCRImageFunctions", {
-      code: new lambda.EcrImageCode(repository, {}),
-      handler: lambda.Handler.FROM_IMAGE,
-      runtime: lambda.Runtime.FROM_IMAGE,
-      timeout: Duration.minutes(15),
-      memorySize: 1024,
-      functionName: "OCRImageFunctions",
-      // TODO parameter storeから取りたい
-      environment: {},
-    });
+    // // Lambda
+    // const ocrLambda = new lambda.Function(this, "OCRImageFunctions", {
+    //   code: new lambda.EcrImageCode(repository, {}),
+    //   handler: lambda.Handler.FROM_IMAGE,
+    //   runtime: lambda.Runtime.FROM_IMAGE,
+    //   timeout: Duration.minutes(15),
+    //   memorySize: 1024,
+    //   functionName: "OCRImageFunctions",
+    //   // TODO parameter storeから取りたい
+    //   environment: {},
+    // });
 
-    // Lambda に S3 へのアクセス権を付与
-    ocrFileBucket.grantRead(ocrLambda);
-    ocrFileBucket.grantWrite(ocrLambda);
+    // // Lambda に S3 へのアクセス権を付与
+    // ocrFileBucket.grantRead(ocrLambda);
+    // ocrFileBucket.grantWrite(ocrLambda);
 
-    // S3 イベント通知を設定（画像がアップロードされたら Lambda をトリガー）
-    // ✅ S3 の特定フォルダ（prefix）に対してのみ Lambda を実行
-    const prefix = "input/"; // 🔹 S3 のフォルダ名（例: input-folder/）
-    ocrFileBucket.addEventNotification(
-      s3.EventType.OBJECT_CREATED,
-      new s3n.LambdaDestination(ocrLambda),
-      { prefix } // 🔹 ここでプレフィックスを指定
-    );
+    // // S3 イベント通知を設定（画像がアップロードされたら Lambda をトリガー）
+    // // ✅ S3 の特定フォルダ（prefix）に対してのみ Lambda を実行
+    // const prefix = "input/"; // 🔹 S3 のフォルダ名（例: input-folder/）
+    // ocrFileBucket.addEventNotification(
+    //   s3.EventType.OBJECT_CREATED,
+    //   new s3n.LambdaDestination(ocrLambda),
+    //   { prefix } // 🔹 ここでプレフィックスを指定
+    // );
   }
 }
